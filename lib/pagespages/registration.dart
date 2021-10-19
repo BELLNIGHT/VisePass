75e84c941343039b83dc7c3d931e84fd74b5cfa7
BELLNIGHT <veer1ramblerru1rambler@yandex.ru>
2021-10-19 05:10:18 +0300

    part 3

--- lib/pagespages/registration.dart
+++ lib/pagespages/registration.dart
@@ -23,17 +23,31 @@ class _AuthPageState extends State<AuthPage> {
       );  
     }
 
+    Widget _input(Icon icon, String hint, TextEditingController controller, bool obsecure)(
+      return Container(
+        padding: EdgeInsets.only(left: 20, right: 20),
+        child: TextField(
+          controller: controller,
+          obscureText: obscure,
+          style: TextStyle(fontSize: 20, color: Colors.blue),
+          decoration: InputDecoration(
+            hintStyle: TextStyle(fontWeight: )
+        )
+      );
+  }
+
+
     Widget _form(String label, void func()){
       return Container(
         child: Column(
           children: <Widget>[
             Padding(
               padding: EdgeInsets.only(bottom: 20, top: 10),
-              child: Text('Login')
+              child: _input()
             ),
             Padding(
               padding: EdgeInsets.only(bottom: 20),
-              child: Text('Password')
+              child: input()
             ),
             SizedBox(height: 20),
             Padding(
@@ -63,3 +77,4 @@ class _AuthPageState extends State<AuthPage> {
     );
   }
 }
+
