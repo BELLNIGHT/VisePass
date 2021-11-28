c092554b6ff443dcd88df9396e1f3e1524561209
BELLNIGHT <veer1ramblerru1rambler@yandex.ru>
2021-11-28 23:48:23 +0300

    Constants of fonts and colors. New plugins

--- lib/pagespages/colorfl.dart
+++ lib/pagespages/colorfl.dart
@@ -2,3 +2,43 @@ import 'package:flutter/material.dart';
 
 const kclf = Color(0xFF281111);
 const lfd = TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0);
+
+const kBContainerHeight = 80.0;
+const kActiveCardColour = Color(0xFF1D1E33);
+const kInactiveCardColour = Color(0xFF111328);
+const kBContainerColour = Color(0xFFEB1555);
+
+const kGTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
+
+const kNTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+
+const kLTextStyle = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultTextStyle = TextStyle(
+  color: Color(0xFF24D876),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBTextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBodyTextStyle = TextStyle(
+  fontSize: 22.0,
+);
