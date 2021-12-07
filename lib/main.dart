98cedc5a4cce3575623724f9040e259a567c91e7
BELLNIGHT <veer1ramblerru1rambler@yandex.ru>
2021-12-07 13:13:14 +0300

    full page with password

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,126 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/pagespages/registration.dart';    
-void main() {
-  runApp(const MyApp());
+import 'package:flutter_application_1/ft/password.dart';
+import 'package:flutter_application_1/pagespages/colorfl.dart';
+import 'package:flutter_application_1/ft/password.dart';
+class AuthPage extends StatefulWidget {
+   AuthPage({Key? key}) : super(key: key);
+
+  @override
+  _AuthPageState createState() => _AuthPageState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+class _AuthPageState extends State<AuthPage> {
+  TextEditingController _loginController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: AuthPage()
+
+    Widget _logo(){
+      return Padding(
+        padding: EdgeInsets.only(top:100),
+        child: Container(
+          child: Align(
+            child: Text('VisePass', style: TextStyle(fontSize: 45, fontWeight: FontWeight.bold, color: Colors.red)
+          )      
+        )
+      )
+      );  
+    }
+
+    Widget _input(Icon icon, String hint, TextEditingController controller, bool obscure){
+      return Container(
+        padding: EdgeInsets.only(left: 20, right: 20),
+        child: TextField(
+          controller: controller,
+          obscureText: obscure,
+          style: TextStyle(fontSize: 20, color: Colors.blue),
+          decoration: InputDecoration(
+            hintStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.blue),
+            hintText: hint,
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.blue, width: 3)
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.blue, width: 1)
+            ),
+            prefixIcon: Padding(
+              padding: EdgeInsets.only(left: 10, right: 10),
+              child: IconTheme(
+                data: IconThemeData(color: Colors.blue),
+                child: icon
+              )
+            )
+          ),
+        ),
+      );
+  }
+
+    Widget _form(String label, void func()){
+      return Container(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(bottom: 20, top: 10),
+              child: _input(Icon(Icons.email), "Email", _loginController, false)
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 20),
+              child: _input(Icon(Icons.lock), "Password", _passwordController, true)
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: EdgeInsets.only(left: 20, right: 20),
+              child: Container(
+                height: 50,
+                width: MediaQuery.of(context).size.width,
+                child: Text(label)
+              )
+            )
+          ],
+        ),
+      );
+    }
+
+
+      
+
+    return Scaffold(
+      
+      body: Column(
+        children: <Widget>[
+          _logo(),
+          _form('', (){}),
+          GestureDetector(
+            onTap: (){
+              Navigator.push(context,MaterialPageRoute(builder: (context) => Passwords()),
+              );
+            },
+            child: Container(
+              height: 80,
+              color: kclf,
+              child: const Center(child: Text('Start', style: lfd,)),
+              )
+            ),
+          GestureDetector(
+            onTap: (){},
+            child: Container(
+              color: Colors.red,
+              child: const Center(child: Text('Forgot your password?', style: lfd,)),
+              )
+            ),
+          GestureDetector(
+            onTap: (){
+
+  
+            },
+            child: Container(
+              color: Colors.blue,
+              child: const Center(child: Text('Register', style: lfd,)),
+              )
+            ),
+        ],
+     )
     );
   }
 }
